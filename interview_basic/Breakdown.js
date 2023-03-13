66ae555cddb1e66ee01d8df641d416aa5758914b
HONGKAI123 <huanghongkai98@GMAIL.COM>
2023-03-12 22:34:40 -0700

    update

--- interview_basic/Breakdown.js
+++ interview_basic/Breakdown.js
@@ -20,5 +20,27 @@ function charCount(str) {
   //return object at the end
   return result;
 }
+//Better way to use for of
+function charCountbetter(str) {
+  //make object to return at end
+  var result = {};
+  //loop over string, for each character
+  // use for of to loop each characterin the string
+  for (var char of str) {
+    char = char.toLowerCase();
+    // console.log(char);  //print h e l l o  if input is hello
+    if (/[a-z0-9]/.test(char)) {
+      if (result[char] > 0) {
+        //can be write simply like
+        //result[char] = result[char]++ || 1
+        result[char]++;
+      } else {
+        result[char] = 1;
+      }
+    }
+  }
+  return result;
+}
 
 console.log(charCount("hello1234123ASD"));
+console.log(charCountbetter("hello123123HHASDQJJ"));
